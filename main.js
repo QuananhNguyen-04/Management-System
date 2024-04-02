f5c3b65b79d4c132ec2c673586bdf96d37a45cc2
haiconkhi <165147723+haiconkhi@users.noreply.github.com>
2024-04-02 18:17:54 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// validation form login
+const inputUsername = document.querySelector(".input-login-username");
+const inputPassword = document.querySelector(".input-login-password");
+const btnLogin = document.querySelector(".login__signInButton");
+
+// validation form login
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputUsername.value === "" || inputPassword.value === "") {
+    alert("Please do not leave it blank");
+  } else {
+    const user = JSON.parse(localStorage.getItem(inputUsername.value));
+    if (
+      user.username === inputUsername.value &&
+      user.password === inputPassword.value
+    ) {
+      alert("Login success");
+      window.location.href = "web.html";
+    } else {
+      alert("Login failed");
+    }
+  }
+});
