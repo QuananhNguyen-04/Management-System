496e6af31c656d1e5c59cb8899d77f4729e83174
QuananhNguyen-04 <sherlingfordk@gmail.com>
2024-04-09 01:33:40 +0700

    Testing firebase getdata

--- BE/firebase_Init.js
+++ BE/firebase_Init.js
@@ -4,10 +4,10 @@
 //! DO XAI FREE NEN HINH NHU CHI TAO DUOC 1 CAI DATABASE THI PHAI =)))))
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
-
+const { initializeApp } = require("firebase/app");
+const { getAnalytics } = require("firebase/analytics");
+const { getDatabase } = require("firebase/database");
+const { getDoc, initializeFirestore, doc, addDoc, getFirestore } = require("firebase/firestore");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
     apiKey: "AIzaSyAFfRTomodMfFqP3JkXPTu34W3vYnKwtmY",
     authDomain: "nha-xe-hanh-phuc-ltnc.firebaseapp.com",
-    databaseURL:"https://nha-xe-hanh-phuc-ltnc-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    databaseURL: "https://nha-xe-hanh-phuc-ltnc-default-rtdb.asia-southeast1.firebasedatabase.app/",
     projectId: "nha-xe-hanh-phuc-ltnc",
     storageBucket: "nha-xe-hanh-phuc-ltnc.appspot.com",
     messagingSenderId: "260388398077",
@@ -25,7 +25,27 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// TODO: do the export for firestore, app, ... and hide the config
+var app;
+let analytics; let firestore;
+if (firebaseConfig?.projectId) {
+    // Initialize Firebase
+    app = initializeApp(firebaseConfig);
 
+    // ERROR: window is not defined (cookies or similar), analytics is not supported
+    if (app.name && typeof window !== 'undefined') {
+        analytics = getAnalytics(app);
+    }
+}
+const store = getFirestore();
+
+// need async function to use await getDoc, or else synchronous will overload the output
+const get = async function () {
+    const cityRef = doc(store, 'users', 'Tam');
+
+    const output = await getDoc(cityRef);
+    console.log(output.data());
+}
+get();
