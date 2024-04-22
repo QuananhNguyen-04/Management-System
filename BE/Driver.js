7b5293ce716e667899c1247eceb5d54c2dc22fbb
QuananhNguyen-04 <sherlingfordk@gmail.com>
2024-04-23 00:49:28 +0700

    Delete untracked or wrong directories files (these files have already been handle to correct dir)

--- BE/Driver.js
+++ BE/Driver.js
@@ -22,20 +22,17 @@ import { isExisted } from "./isExisted.js";
 
 
 //          SOME FUNCTION NEEDED TO OPTIMIZE CODING: START
-function setExpiry(driver)
-{
+function setExpiry(driver) {
     let expiration;
-    if(isExisted(driver.license)) expiration=driver.license.getExpiryTime();
-    if(isExisted(expiration))
-    {
-        driver.license.expiry=setTimeout(()=>{
-            driver.license.updateLicenseInfo('expiryDate','Expired');
+    if (isExisted(driver.license)) expiration = driver.license.getExpiryTime();
+    if (isExisted(expiration)) {
+        driver.license.expiry = setTimeout(() => {
+            driver.license.updateLicenseInfo('expiryDate', 'Expired');
             driver.update();
-        },expiration);
+        }, expiration);
     }
 }
-function clearExpiry(driver)
-{
+function clearExpiry(driver) {
     clearTimeout(driver.expiry);
 }
 /**function isObjectExisted(S)
@@ -49,13 +46,11 @@ function clearExpiry(driver)
     }
     return false;
 }**/
-function compareDriver(a, b)
-{
-    return (a.id==b.id && a.license==b.license && a.yearsOfExp==b.yearsOfExp && a.status==b.status && a.recentTrip==b.recentTrip && a.workingTime==b.workingTime && a.efficiency==b.efficiency); 
+function compareDriver(a, b) {
+    return (a.id == b.id && a.license == b.license && a.yearsOfExp == b.yearsOfExp && a.status == b.status && a.recentTrip == b.recentTrip && a.workingTime == b.workingTime && a.efficiency == b.efficiency);
 }
-function setInfo(info, value)
-{
-    if(isExisted(value)) info = value;
+function setInfo(info, value) {
+    if (isExisted(value)) info = value;
     else info = 'N/A';
     return info;
 }
@@ -64,43 +59,37 @@ function setInfo(info, value)
 
 
 
-class driverLicense
-{
-    constructor(id, tier, img, expiryDate)
-    {
-        this.id = setInfo(this.id,id);
-        this.tier = setInfo(this.tier,tier);
-        this.img = setInfo(this.img,img);
-        this.expiryDate = setInfo(this.expiryDate,expiryDate);
-        this.expiry= undefined;
+class driverLicense {
+    constructor(id, tier, img, expiryDate) {
+        this.id = setInfo(this.id, id);
+        this.tier = setInfo(this.tier, tier);
+        this.img = setInfo(this.img, img);
+        this.expiryDate = setInfo(this.expiryDate, expiryDate);
+        this.expiry = undefined;
     }
 
-    getExpiryTime()
-    {
-        if(isExisted(this.expiryDate)&&this.expiryDate!=='Expired')
-        {
-            let expiration=Date.parse(this.expiryDate) - new Date();
-            if (expiration<2000) expiration=2000;
+    getExpiryTime() {
+        if (isExisted(this.expiryDate) && this.expiryDate !== 'Expired') {
+            let expiration = Date.parse(this.expiryDate) - new Date();
+            if (expiration < 2000) expiration = 2000;
             return expiration;
         }
         else return 'N/A';
     }
 
-    updateLicenseInfo(infoType,newValue)
-    {
-        switch(infoType)
-        {
+    updateLicenseInfo(infoType, newValue) {
+        switch (infoType) {
             case 'id':
-                this.id=setInfo(this.id,newValue);
+                this.id = setInfo(this.id, newValue);
                 break;
             case 'tier':
-                this.tier=setInfo(this.tier,newValue);
+                this.tier = setInfo(this.tier, newValue);
                 break;
             case 'img':
-                this.img=setInfo(this.img,newValue);
+                this.img = setInfo(this.img, newValue);
                 break;
             case 'expiryDate':
-                this.expiryDate=setInfo(this.expiryDate,newValue);
+                this.expiryDate = setInfo(this.expiryDate, newValue);
                 break;
             default:
                 console.log('Invalid info type.');
@@ -108,30 +97,26 @@ class driverLicense
     }
 }
 
-class driver 
-{
-    constructor(id, license, yearsOfExp, status, recentTrip, workingTime, efficiency/*,other*/)
-    {
-        this.id = setInfo(this.id,id);
+class driver {
+    constructor(id, license, yearsOfExp, status, recentTrip, workingTime, efficiency/*,other*/) {
+        this.id = setInfo(this.id, id);
         //this.id = parseInt(this.id,10);
         this.license = new driverLicense();
         this.license = setInfo(this.license, license);
-        
+
         this.yearsOfExp = setInfo(this.yearsOfExp, yearsOfExp);
-        this.status = setInfo(this.status,status);
-        this.recentTrip = setInfo(this.recentTrip,recentTrip);
-        this.workingTime = setInfo(this.workingTime,workingTime);
-        this.efficiency = setInfo(this.efficiency,efficiency);
+        this.status = setInfo(this.status, status);
+        this.recentTrip = setInfo(this.recentTrip, recentTrip);
+        this.workingTime = setInfo(this.workingTime, workingTime);
+        this.efficiency = setInfo(this.efficiency, efficiency);
         //this.other = setInfo(this.other,other);
-        if(isExisted(license))
-        {
+        if (isExisted(license)) {
             setExpiry(this);
         }
         console.log('Driver created.');
     }
 
-    driverInfo()
-    {
+    driverInfo() {
         console.log('ID: ', this.id);
         console.log('License: ', this.license);
         console.log('Years of Experiment: ', this.yearsOfExp);
@@ -142,8 +127,7 @@ class driver
         //console.log(this);
     }
 
-    carType()
-    {
+    carType() {
         //Car Type sẽ thuộc {A1,A2,A3,A4,B1,B2,B3,...}
         //Khi kiểm tra xem tài xế có thể lái một loại xe hay không, hãy so sánh giá trị này với giá trị yêu cầu tối thiểu của xe
         //<=carType() nghĩa là tài xế có thê lái xe đó
@@ -151,46 +135,42 @@ class driver
         return this.license.tier;
     }
 
-    updateDriverInfo(infoType, newValue)
-    {
-        switch(infoType)
-        {
+    updateDriverInfo(infoType, newValue) {
+        switch (infoType) {
             case 'id':
-                this.id=setInfo(this.id,newValue);
+                this.id = setInfo(this.id, newValue);
                 break;
             case 'license':
-                this.license=setInfo(this.license,newValue);
+                this.license = setInfo(this.license, newValue);
                 clearExpiry(this);
                 setExpiry(this);
                 break;
             case 'yearsOfExp':
-                this.yearsOfExp=setInfo(this.yearsOfExp,newValue);
+                this.yearsOfExp = setInfo(this.yearsOfExp, newValue);
                 break;
             case 'status':
-                this.status=setInfo(this.status,newValue);
+                this.status = setInfo(this.status, newValue);
                 break;
             case 'recentTrip':
-                this.recentTrip=setInfo(this.recentTrip,newValue);
+                this.recentTrip = setInfo(this.recentTrip, newValue);
                 break;
             case 'workingTime':
-                this.workingTime=setInfo(this.workingTime,newValue);
+                this.workingTime = setInfo(this.workingTime, newValue);
                 break;
             case 'efficiency':
-                this.efficiency=setInfo(this.efficiency,newValue);
+                this.efficiency = setInfo(this.efficiency, newValue);
                 break;
             default:
                 console.log('Invalid info type.');
         }
     }
 
-    update()
-    {
-        let temp=searchDriver(this);
-        if(isExisted(temp))
-        {
-            temp.forEach(doc => {editDriver(doc.id,this);});
+    update() {
+        let temp = searchDriver(this);
+        if (isExisted(temp)) {
+            temp.forEach(doc => { editDriver(doc.id, this); });
         }
         else console.log('Fail: Undefined driver.');
     }
 }
-export {driver,compareDriver,setInfo,driverLicense,/*isObjectExisted,*/setExpiry,clearExpiry};
+export { driver, compareDriver, setInfo, driverLicense,/*isObjectExisted,*/setExpiry, clearExpiry };
--- FE/Driver/add_drive.js
+++ FE/Driver/add_drive.js
@@ -1,23 +1,23 @@
-var Name_Driver=document.getElementsByName("Name_Driver").values;
-var DoB= document.getElementsByName("DoB_Driver").values;
-var Phone= document.getElementsByName("Phone_Driver").values;
-var ID= document.getElementsByName("ID_Driver").values;
-var IMG_Driver= document.getElementsByName("IMG_Driver").values;
-var Lisence_Rank= document.getElementsByName("Lisence_Rank").values;
-var Lisence_Id= document.getElementsByName("Lisence_ID").values;
-var Lisence_BD= document.getElementsByName("Lisence_BD").values;
-var Lisence_Experies= document.getElementsByName("Lisence_Experies").values;
-var Front_License= document.getElementsByName("Front_License").values;
-var Back_Lisence= document.getElementsByName("Back_Lisence").values;
+var Name_Driver = document.getElementsByName("Name_Driver").values;
+var DoB = document.getElementsByName("DoB_Driver").values;
+var Phone = document.getElementsByName("Phone_Driver").values;
+var ID = document.getElementsByName("ID_Driver").values;
+var IMG_Driver = document.getElementsByName("IMG_Driver").values;
+var Lisence_Rank = document.getElementsByName("Lisence_Rank").values;
+var Lisence_Id = document.getElementsByName("Lisence_ID").values;
+var Lisence_BD = document.getElementsByName("Lisence_BD").values;
+var Lisence_Experies = document.getElementsByName("Lisence_Experies").values;
+var Front_License = document.getElementsByName("Front_License").values;
+var Back_Lisence = document.getElementsByName("Back_Lisence").values;
 
 import { driver_wrapper } from "../../BE/Driver_Wrapper.js"
-import { license} from "../../BE/Driver.js"
+import { driverLicense } from "../../BE/Driver.js"
+wrap = new driver_wrapper();
 
-document.getElementById("add-driver").
-addEventListener("click", async function () {
-    wrap = new driver_wrapper();
-    li = new license(Lisence_Id, Lisence_Rank, Lisence_BD, Lisence_Experies, Front_License, Back_Lisence)
-    wrap.create(Name_Driver, DoB, Phone, ID, IMG_Driver, li);
+document.getElementById("add_driver").
+    addEventListener("click", async function () {
+        li = new driverLicense(Lisence_Id, Lisence_Rank, Lisence_BD, Lisence_Experies, Front_License, Back_Lisence)
+        wrap.create(Name_Driver, DoB, Phone, ID, IMG_Driver, li);
 
-    // wrap.create(ID, , )
-})
+        // wrap.create(ID, , )
+    })
